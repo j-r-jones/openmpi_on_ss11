5107413928365074d525bc8eec958728b3e84807
alazzaro <alfio.lazzaro@gmail.com>
2026-03-03 12:53:28 +0200

    Refresh p2p 1N results with cray-mpich

--- osu/craype/pt2pt/run_singlenode.sh
+++ osu/craype/pt2pt/run_singlenode.sh
@@ -15,12 +15,30 @@ source $ROOT_DIR/sourceme_craympi.sh
 export MPICH_GPU_SUPPORT_ENABLED=1
 export MPICH_SMP_SINGLE_COPY_MODE=XPMEM
 export FI_CXI_RX_MATCH_MODE=hybrid
+#export FI_MR_CACHE_MONITOR=kdreg2 # no performance contribution
+#export GTL_DISABLE_HSA_CACHE=1 # not available in 9.0.1
+#export MPICH_GPU_IPC_ENABLED=0
+#export MPICH_GPU_IPC_THRESHOLD=524288 # beneficial for `-b multiple`
+#export MPICH_GPU_IPC_THRESHOLD=32768 # beneficial for `-b single`
+#export MPICH_GPU_IPC_CACHE_MAX_SIZE=100 # improves performance and makes working with large buffers
 # export FI_LOG_LEVEL=debug
 
+export MPICH_VERSION_DISPLAY=1
+export GTL_VERSION_DISPLAY=1
+
+echo "============"
+cat $0
+echo "============"
+
+env
+
 CMDS=("osu_bibw -b multiple D D" "osu_latency D D" "osu_bibw -b multiple H H" "osu_latency H H")
+#CMDS=("osu_bibw -W 32 -b multiple D D")
+#CMDS=("osu_bibw -b multiple D D")
+#CMDS=("osu_bibw D D")
 #CMDS=("osu_bibw D D" "osu_latency D D" "osu_bibw H H" "osu_latency H H")
 for cmd in "${CMDS[@]}"; do
-    run_osu_cmd "$cmd" "mpi/pt2pt" "_singlenode"
+    run_osu_cmd "$cmd" "mpi/pt2pt" "_singlenode_$SLURM_JOB_ID"
 done
 
 # NCCL
