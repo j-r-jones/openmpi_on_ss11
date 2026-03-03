972cedd8cf0da1020f2555d9ba9cb4a438e31227
alazzaro <alfio.lazzaro@gmail.com>
2026-03-03 15:22:41 +0200

    Update OpenMPI env variables for LNX

--- affinity/run_ompi.sh
+++ affinity/run_ompi.sh
@@ -9,11 +9,17 @@ cd $SCRIPT_DIR
 # OFI env
 export FI_LNX_PROV_LINKS="shm+cxi"
 export FI_SHM_USE_XPMEM=1
+export FI_PROVIDER=lnx
+export FI_CXI_RX_MATCH_MODE=hybrid
 
 # OpenMPI env
 export OMPI_MCA_mtl=ofi
-export OMPI_MCA_opal_common_ofi_provider_include="shm+cxi:lnx"
+export OMPI_MCA_opal_common_ofi_provider_include=lnx
+export OMPI_MCA_mtl_ofi_av=table
+export OMPI_MCA_pml=cm
 export OMPI_MCA_smsc=xpmem
+export PRTE_MCA_ras_base_launch_orted_on_hn=1
+export PMIX_MCA_gds=^shmem2
 
 APP=xthi
 
